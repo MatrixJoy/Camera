ebbd53f0c1b2029f9fe22c194dc002fed8be56ff
matrixJoy <785375121@qq.com>
2021-03-28 19:58:37 +0800

    add a AudioTrack to play pcm

--- codec/src/main/java/top/catnemo/codec/decode/AudioDecoder.kt
+++ codec/src/main/java/top/catnemo/codec/decode/AudioDecoder.kt
@@ -1,5 +1,6 @@
 package top.catnemo.codec.decode
 
+import android.media.AudioFormat
 import android.media.MediaCodec
 import android.media.MediaExtractor
 import android.media.MediaFormat
@@ -11,93 +12,135 @@ import java.nio.ByteBuffer
  */
 class AudioDecoder {
 
-    private var stop = false
-
     companion object {
         private const val TAG = "AudioDecoder"
     }
 
-    fun startDecode(path: String, callback: (ByteBuffer) -> Unit) {
-        val mediaExtractor = MediaExtractor()
-        mediaExtractor.setDataSource(path)
-        val trackCount = mediaExtractor.trackCount
-        var format: MediaFormat? = null
-        var mime: String = ""
-        var index = 0
-        for (i in 0 until trackCount) {
-            format = mediaExtractor.getTrackFormat(i)
-            mime = format.getString(MediaFormat.KEY_MIME)
-            if (mime.startsWith("audio/")) {
-                index = i
-                break
-            }
-        }
+    private var audioDecodeThread: AudioDecodeThread? = null
 
-        format ?: return
+    fun initAudioDecode(path: String, audioDecodeCallBack: AudioDecodeCallBack) {
+        audioDecodeThread = AudioDecodeThread(path, audioDecodeCallBack)
+    }
 
-        val codec = MediaCodec.createDecoderByType(mime)
-        codec.configure(format, null, null, 0)
-        codec.start()
-        val codecInputBuffers = codec.inputBuffers
-        var codecOutputBuffers = codec.outputBuffers
-
-        mediaExtractor.selectTrack(index)
-
-        val timeOutUs = 5000L
-        val info = MediaCodec.BufferInfo()
-
-        var inputEOS = false
-        var outputEOS = false
-
-        var startTime = System.currentTimeMillis()
-        while (!outputEOS && !stop) {
-            if (!inputEOS) {
-                val inputBufferIndex = codec.dequeueInputBuffer(timeOutUs)
-                if (inputBufferIndex >= 0) {
-                    val dstBuf = codecInputBuffers[inputBufferIndex]
-                    var sampleSize = mediaExtractor.readSampleData(dstBuf, 0)
-                    var presentationTimeUs = 0L
-                    if (sampleSize < 0) {
-                        inputEOS = true
-                        sampleSize = 0
-                    } else {
-                        presentationTimeUs = mediaExtractor.sampleTime
-                    }
-                    codec.queueInputBuffer(inputBufferIndex, 0, sampleSize, presentationTimeUs,
-                            if (inputEOS) MediaCodec.BUFFER_FLAG_END_OF_STREAM else 0)
-                    if (!inputEOS) {
-                        Log.d(TAG, "presentationTimeUs $presentationTimeUs")
-                        mediaExtractor.seekTo(presentationTimeUs + 5 * 1000 * 1000, MediaExtractor.SEEK_TO_CLOSEST_SYNC)
-                        mediaExtractor.advance()
-                    }
-                }
-            }
-
-            var res = codec.dequeueOutputBuffer(info, timeOutUs)
-            if (res > 0) {
-                val outputBufIndex = res
-                val buf = codecOutputBuffers[outputBufIndex]
-                callback(buf)
-                codec.releaseOutputBuffer(outputBufIndex, false)
-                if (info.flags and MediaCodec.BUFFER_FLAG_END_OF_STREAM != 0) {
-                    outputEOS = true
-                }
-            } else if (res == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
-                codecOutputBuffers = codec.outputBuffers
-            } else if (res == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
-                val format = codec.outputFormat
-                Log.d(TAG, "output format changed $format")
-            } else {
-                Log.d(TAG, "unknown error $res")
-            }
-
-        }
-        codec.stop()
-        codec.release()
-        Log.d(TAG, "costTime ${System.currentTimeMillis() - startTime}")
+    fun startDecode() {
+        audioDecodeThread?.start()
     }
 
     fun stop() {
-        stop = true
+        audioDecodeThread?.stopDecode()
+    }
+
+    private class AudioDecodeThread(val path: String, val audioDecodeCallBack: AudioDecodeCallBack) : Thread() {
+
+        private var stop = false
+        private var isStart = false
+
+        companion object {
+            private const val AUDIO_PREFIX = "audio/"
+        }
+
+        override fun run() {
+            startDecode(path, audioDecodeCallBack)
+        }
+
+        fun startDecode(path: String, callback: AudioDecodeCallBack) {
+            val mediaExtractor = MediaExtractor()
+            mediaExtractor.setDataSource(path)
+            val trackCount = mediaExtractor.trackCount
+            var format: MediaFormat? = null
+            var mime = ""
+            var index = 0
+            for (i in 0 until trackCount) {
+                format = mediaExtractor.getTrackFormat(i)
+                mime = format.getString(MediaFormat.KEY_MIME)
+                if (mime.startsWith(AUDIO_PREFIX)) {
+                    index = i
+                    break
+                }
+            }
+            format ?: return
+
+            isStart = true
+
+            val sampleRate = format.getInteger(MediaFormat.KEY_SAMPLE_RATE)
+            val audioFormat = AudioFormat.ENCODING_PCM_16BIT
+            val channelCount = format.getInteger(MediaFormat.KEY_CHANNEL_COUNT)
+            callback.startDecode(sampleRate, audioFormat, channelCount)
+
+            val codec = MediaCodec.createDecoderByType(mime)
+            codec.configure(format, null, null, 0)
+            codec.start()
+            val codecInputBuffers = codec.inputBuffers
+            var codecOutputBuffers = codec.outputBuffers
+
+            mediaExtractor.selectTrack(index)
+
+            val timeOutUs = 5000L
+            val info = MediaCodec.BufferInfo()
+
+            var inputEOS = false
+            var outputEOS = false
+
+            val startTime = System.currentTimeMillis()
+            while (!outputEOS && !stop) {
+                if (!inputEOS) {
+                    val inputBufferIndex = codec.dequeueInputBuffer(timeOutUs)
+                    if (inputBufferIndex >= 0) {
+                        val dstBuf = codecInputBuffers[inputBufferIndex]
+                        var sampleSize = mediaExtractor.readSampleData(dstBuf, 0)
+                        var presentationTimeUs = 0L
+                        if (sampleSize < 0) {
+                            inputEOS = true
+                            sampleSize = 0
+                        } else {
+                            presentationTimeUs = mediaExtractor.sampleTime
+                        }
+                        codec.queueInputBuffer(inputBufferIndex, 0, sampleSize, presentationTimeUs,
+                                if (inputEOS) MediaCodec.BUFFER_FLAG_END_OF_STREAM else 0)
+                        if (!inputEOS) {
+                            Log.d(TAG, "presentationTimeUs $presentationTimeUs")
+                            mediaExtractor.advance()
+                        }
+                    }
+                }
+
+                val res = codec.dequeueOutputBuffer(info, timeOutUs)
+                if (res > 0) {
+                    val buf = codecOutputBuffers[res]
+                    callback.decoding(buf)
+                    codec.releaseOutputBuffer(res, false)
+                    if (info.flags and MediaCodec.BUFFER_FLAG_END_OF_STREAM != 0) {
+                        outputEOS = true
+                    }
+                } else if (res == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
+                    codecOutputBuffers = codec.outputBuffers
+                } else if (res == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+                    val format = codec.outputFormat
+                    Log.d(TAG, "output format changed $format")
+                } else {
+                    Log.d(TAG, "unknown error $res")
+                }
+            }
+            codec.stop()
+            codec.release()
+            mediaExtractor.release()
+            callback.decodeEnd()
+            Log.d(TAG, "costTime ${System.currentTimeMillis() - startTime}")
+        }
+
+        fun stopDecode() {
+            if (!isStart) {
+                return
+            }
+            stop = true
+        }
+    }
+
+    interface AudioDecodeCallBack {
+        fun startDecode(sampleRate: Int, enCoding: Int, channelCount: Int)
+
+        fun decoding(shortBuffer: ByteBuffer)
+
+        fun decodeEnd()
     }
 }
--- demo/src/main/java/com/catnemo/avstudy/audo/AudioSampleActivity.kt
+++ demo/src/main/java/com/catnemo/avstudy/audo/AudioSampleActivity.kt
@@ -1,15 +1,93 @@
 package com.catnemo.avstudy.audo
 
+import android.media.AudioFormat
+import android.media.AudioManager
+import android.media.AudioTrack
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import com.catnemo.avstudy.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import top.catnemo.codec.decode.AudioDecoder
+import java.io.File
+import java.io.FileOutputStream
+import java.nio.ByteBuffer
 
 class AudioSampleActivity : AppCompatActivity() {
+    companion object{
+        private const val TEST_FILE = "001JTjCp0Xvv9D.m4a"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_audio_sample)
+        val audioDecode = AudioDecoder()
+        val btnDecode = findViewById<Button>(R.id.btn_decode)
+        val destFile = coyTestFile(btnDecode)
+        btnDecode.setOnClickListener {
+            startDecode(audioDecode, destFile)
+        }
+
+        findViewById<Button>(R.id.btn_decode_stop).setOnClickListener {
+            audioDecode.stop()
+        }
+    }
+
+    private fun startDecode(audioDecode: AudioDecoder, destFile: File) {
+        var audioTrack: AudioTrack? = null
+        audioDecode.initAudioDecode(destFile.path, object : AudioDecoder.AudioDecodeCallBack {
+            override fun startDecode(sampleRate: Int, audioFormat: Int, channelCount: Int) {
+                val audioChannelConfig = if (channelCount == 1) AudioFormat.CHANNEL_OUT_MONO else AudioFormat.CHANNEL_OUT_STEREO
+                val bufferSize = AudioTrack.getMinBufferSize(sampleRate, audioChannelConfig, audioFormat)
+                audioTrack = AudioTrack(AudioManager.STREAM_MUSIC, sampleRate, audioChannelConfig,
+                        audioFormat, bufferSize, AudioTrack.MODE_STREAM)
+            }
+
+            override fun decoding(byteBuffer: ByteBuffer) {
+                val shortBuffer = byteBuffer.asShortBuffer()
+                val length = shortBuffer.limit()
+                val samples = ShortArray(length)
+                shortBuffer.get(samples, 0, length)
+                if (AudioTrack.SUCCESS == audioTrack?.write(samples, 0, length)) {
+                    audioTrack?.play()
+                }
+            }
+
+            override fun decodeEnd() {
+                if (audioTrack?.playState == AudioTrack.PLAYSTATE_PLAYING) {
+                    audioTrack?.stop()
+                }
+                audioTrack?.release()
+            }
+        })
+        audioDecode.startDecode()
+    }
+
+    private fun coyTestFile(btnDecode: Button): File {
+        val destFile = File(filesDir.path + File.separator + TEST_FILE)
+        if (!destFile.exists()) {
+            btnDecode.isEnabled = false
+            CoroutineScope(Dispatchers.IO).launch {
+                assets.open(TEST_FILE).use { input ->
+                    FileOutputStream(destFile).use { outPut ->
+                        val byteArray = ByteArray(2048)
+                        while (input.read(byteArray) != -1) {
+                            outPut.write(byteArray)
+                            withContext(Dispatchers.Main) {
+                                btnDecode.text = "转码中..."
+                            }
+                        }
+                    }
+                }
+
+                withContext(Dispatchers.Main) {
+                    btnDecode.isEnabled = true
+                    btnDecode.text = getString(R.string.start_decode)
+                }
+            }
+        }
+        return destFile
     }
 }
